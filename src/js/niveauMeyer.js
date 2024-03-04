78f080caf047d0b06b8a7e612e05f9367161a572
ana_rita nazare_pinheiro <anarita.nazarepinheiro@epfedu.fr>
2024-03-04 19:09:26 +0100

    AJOUT MAP SALLE

--- src/js/niveauMeyer.js
+++ src/js/niveauMeyer.js
@@ -1,5 +1,8 @@
 import * as fct from "/src/js/fonctions.js";
 
+var player;
+var cursors;
+
 export default class niveauMeyer extends Phaser.Scene {
   // constructeur de la classe
   constructor() {
@@ -7,52 +10,118 @@ export default class niveauMeyer extends Phaser.Scene {
       key: "niveauMeyer" //  ici on précise le nom de la classe en tant qu'identifiant
     });
   }
+
   preload() {
+    // ajout perso
+    this.load.spritesheet("dude", "src/assets/dude.png", { frameWidth: 32, frameHeight: 48 });
+
+    // chargement tuiles de jeu
+    this.load.image("tuiles1", "src/assets/tilesheet_complete.png");
+
+
+    // chargement de la carte
+    this.load.tilemapTiledJSON("classe", "src/assets/MapSalleCours.json");
   }
 
   create() {
-    fct.doNothing();
-    fct.doAlsoNothing();
+    //chargement de la carte et des jeux de tuiles 
+    const CarteDeLaClasse = this.add.tilemap("classe");
+    const tileset = CarteDeLaClasse.addTilesetImage("tilesheet_complete","tuiles1");
+    // chargement des calques qui constituent le background de la pièce
+    const calque_background = CarteDeLaClasse.createLayer(
+      "Calque de Tuiles 1",
+      tileset
+    );
+    const calque_background2 = CarteDeLaClasse.createLayer(
+      "Calque de Tuiles 2",
+      tileset
+    );
+    const calque_background3 = CarteDeLaClasse.createLayer(
+      "Calque de Tuiles 3",
+      tileset
+    );
+    calque_background.setCollisionByProperty({ estSolide: true });
 
-    this.add.image(400, 300, "img_ciel");
-    this.groupe_plateformes = this.physics.add.staticGroup();
-    this.groupe_plateformes.create(200, 584, "img_plateforme");
-    this.groupe_plateformes.create(600, 584, "img_plateforme");
-    // ajout d'un texte distintcif  du niveau
-    this.add.text(400, 100, "Vous êtes dans le niveau 1", {
-      fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
-      fontSize: "22pt"
-    });
+    
+  
+   // création du personnage de jeu et positionnement
+   player = this.physics.add.sprite(100, 400, "dude");
+   player.setBounce(0.2);
+ 
+ 
+   // animation pour tourner à gauche
+   this.anims.create({
+     key: "left",
+     frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+     frameRate: 10,
+     repeat: -1
+   });
+ 
+   // animation lorsque le personnage n'avance pas
+   this.anims.create({
+     key: "turn",
+     frames: [{ key: "dude", frame: 4 }],
+     frameRate: 20
+   });
+ 
+   // animation pour tourner à droite
+   this.anims.create({
+     key: "right",
+     frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+     frameRate: 10,
+     repeat: -1
+   });
+ 
+   // création d'un écouteur sur le clavier
+   cursors = this.input.keyboard.createCursorKeys();
+   
+    // ajout du modèle de collision entre le personnage et le monde
+    player.setCollideWorldBounds(true);
 
-    this.porte_retour = this.physics.add.staticSprite(100, 550, "img_porte1");
+    this.physics.world.setBounds(0, 0, 3200, 640);
+    //  ajout du champs de la caméra de taille identique à celle du monde
+    this.cameras.main.setBounds(0, 0, 3200, 640);
+    // ancrage de la caméra sur le joueur
+    this.cameras.main.startFollow(player);
 
-    this.player = this.physics.add.sprite(100, 450, "img_perso");
-    this.player.refreshBody();
-    this.player.setBounce(0.2);
-    this.player.setCollideWorldBounds(true);
-    this.clavier = this.input.keyboard.createCursorKeys();
-    this.physics.add.collider(this.player, this.groupe_plateformes);
+    // ajout du modèle de collision entre le personnage et le monde
+    player.setCollideWorldBounds(true);
+
+    // ajout d'une collision entre le joueur et le calque plateformes
+    this.physics.add.collider(player, calque_background);
+
+
   }
 
   update() {
-    if (this.clavier.left.isDown) {
-      this.player.setVelocityX(-160);
-      this.player.anims.play("anim_tourne_gauche", true);
-    } else if (this.clavier.right.isDown) {
-      this.player.setVelocityX(160);
-      this.player.anims.play("anim_tourne_droite", true);
-    } else {
-      this.player.setVelocityX(0);
-      this.player.anims.play("anim_face");
-    }
-    if (this.clavier.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-330);
-    }
+    // définitinon des mouvements du personnage
 
-    if (Phaser.Input.Keyboard.JustDown(this.clavier.space) == true) {
-      if (this.physics.overlap(this.player, this.porte_retour)) {
-        this.scene.switch("selection");
-      }
-    }
+  if (cursors.up.isDown) {
+    player.setVelocityY(-160);
+    // à droite
+  } else if (cursors.down.isDown) {
+    player.setVelocityY(160);
+  }
+
+  // a gauche
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play("left", true);
+
+    // à droite
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+    player.anims.play("right", true);
+  }
+  // immoobile
+  else {
+    player.setVelocityX(0);
+    player.anims.play("turn");
+  }
+  // en saut (important : blocked doown au lieu de tuoching down)
+  if (cursors.up.isDown && player.body.blocked.down) {
+    player.setVelocityY(-200);
+  }
   }
 }
+
