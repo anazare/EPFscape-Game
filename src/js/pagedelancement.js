ddd2d42fe0bbf28064014b3ed451dc08dfa9f20c
anis eljeljal <anis.eljeljal@epfedu.fr>
2024-03-06 19:02:09 +0100

    gg

--- src/js/pagedelancement.js
+++ src/js/pagedelancement.js
@@ -11,12 +11,28 @@ export default class Pagedelancement extends Phaser.Scene {
     this.load.image("EPF", "src/assets/play.jpg");
     this.load.image("img_plateforme", "src/assets/platform.png");
     this.load.image("Start1", "src/assets/boutonstart1.png");
+    this.load.image("Exit", "src/assets/exitjeu.png");
   }
 
   create() {
     // Ajouter l'image de fond
     this.add.image(400, 300, "EPF").setScale(0.57);
-    
+
+    const bouton_play1 = this.add.image(750, 50, "Exit").setScale(0.1); // Réglez la valeur selon vos besoins
+    bouton_play1.setInteractive();
+
+    bouton_play1.on("pointerover", () => {
+      bouton_play1.setTint(0xC0C0C0);
+    });
+
+    bouton_play1.on("pointerout", () => {
+      bouton_play1.clearTint();
+    });
+
+    bouton_play1.on("pointerup", () => {
+      this.scene.stop("pagedelancement");
+      window.close();
+    });
     // Appeler la méthode pour afficher le bouton
     this.afficherBouton();
   }
@@ -25,19 +41,21 @@ export default class Pagedelancement extends Phaser.Scene {
 
   afficherBouton() {
     // Déclarer la variable bouton_play
-    const bouton_play = this.add.image(400, 480, "Start1").setScale(0.3).setAlpha(0.01); // Réglez la valeur selon vos besoins
+    const bouton_play = this.add.image(400, 480, "Start1").setScale(0.3).setAlpha(0.2);; // Réglez la valeur selon vos besoins
     bouton_play.setInteractive();
 
     bouton_play.on("pointerover", () => {
       bouton_play.setTint(0xC0C0C0);
+      bouton_play.setAlpha(0);
     });
 
     bouton_play.on("pointerout", () => {
+      bouton_play.setAlpha(0.2);
       bouton_play.clearTint();
     });
 
     bouton_play.on("pointerup", () => {
-      this.scene.start("selection");
+      this.scene.start("chargementP");
     });
   }
 }
