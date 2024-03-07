2e2fd6cf22c75cf7f71857d59d46d5c14a215aec
ana_rita nazare_pinheiro <anarita.nazarepinheiro@epfedu.fr>
2024-03-07 21:56:56 +0100

    RETOUR à 8 pts

--- src/js/chargementP.js
+++ src/js/chargementP.js
@@ -46,7 +46,7 @@ export default class chargementP extends Phaser.Scene {
   }
 
   onVideoEnded() {
-    if (gameData.g == 1){
+    if (gameData.g == 3){
         this.scene.switch("End");   
         this.scene.stop();
         console.log("arret scene et switch vers end"); 
